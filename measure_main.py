092e34d850b35650a39143ed9f6538220964ea60
marszaj <jakubmarszalek94@gmail.com>
2020-05-17 19:48:18 +0100

    repair voltage measure, enable autostart

--- measure_main.py
+++ measure_main.py
@@ -8,15 +8,23 @@ import voltage_measure
 import technical_recorder
 
 perioud = 2
-v=5
 start_time = time.time()
 result = {}
+
+start_voltage_value = 1 # voults
+start_sleep_perioud = 0.05
+
+stop_current_value = 0.1
+stop_voltage_value = 1
+
+result_folder_name = "results"
 
 
 def create_result_name():
     time_to_name = time.gmtime()
     name_file = f"{time_to_name.tm_year}_{time_to_name.tm_mon}_{time_to_name.tm_mday}_{time_to_name.tm_hour + 2}_{time_to_name.tm_min}_{time_to_name.tm_sec}"
     return name_file
+
 
 def create_report(result):
     number_of_measure = len(result) - 1
@@ -47,6 +55,11 @@ def create_report(result):
 
 
 def main():
+    while 1:
+        if voltage_measure.main() > start_voltage_value:
+            print(f"Detected voltage bigger then {start_voltage_value}V, measure start:")
+            break
+        time.sleep(start_sleep_perioud)
     count = 0 
     while 1:
         time.sleep(perioud)
@@ -63,13 +76,15 @@ def main():
         measure_time = time.time() - start_time
         result[count]={"current": a, "voltage": v, "power": p, "energy_in_perioud": w_temp, "all_energy": w_all, "measure_time": measure_time, "RAM using %": ram}
         print(result[count])
-        if result[count]["current"] < 0.1:
+        if (result[count]["current"] < stop_current_value) or (result[count]["voltage"] < stop_voltage_value):
             output_name = create_result_name()
-            os.mkdir(f"results/{output_name}")
-            with open(f"results/{output_name}/measure_data.json", "w") as outfile:
+            script_localization = os.path.dirname(os.path.realpath(__file__))
+            result_path = os.path.join(script_localization, result_folder_name, output_name)
+            os.mkdir(result_path)
+            with open(os.path.join(result_path, "measure_data.json"), "w") as outfile:
                 json.dump(result, outfile)
             final_report = create_report(result)
-            with open(f"results/{output_name}/final_report.json", "w") as outfile:
+            with open(os.path.join(result_path, "final_report.json"), "w") as outfile:
                 json.dump(final_report, outfile)
             break
 
--- voltage_measure.py
+++ voltage_measure.py
@@ -10,12 +10,15 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 voltage_pin = 0
 v_ref = 5.1
 bit_convertion = v_ref/1024
+voltage_lost_factor = 0.93
 
 def main():
     bit_voltage_value = mcp.read_adc(voltage_pin)
+    #print(bit_voltage_value)
     voltage_value = bit_voltage_value * bit_convertion
+    voltage_value = voltage_value / voltage_lost_factor
     return voltage_value
 
 if __name__ == "__main__":
-    print(main())
+    main()
 
